d609e05605745f8a9607564944072b0734521751
adriantr0808 <adriantr.gt@gmail.com>
2021-11-05 14:22:26 +0100

    update

--- src/components/EmpleadosServiciosOficios.vue
+++ src/components/EmpleadosServiciosOficios.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="container m-4">
     <h1>Oficios con servicios</h1>
-    <select></select>
+    <form method="post" v-on:change.prevent="mostrarEmpleados(oficio)">
+      <select v-model="oficio">
+        <option v-for="(o, index) in oficios" :key="index" :value="o">
+          {{ o }}
+        </option>
+      </select>
+    </form>
+    <div>
+      <h1>{{ empleados.apellido }}</h1>
+    </div>
   </div>
 </template>
 <script>
+import ServiceOficios from "./../services/ServiceOficios";
+
+const service = new ServiceOficios();
 export default {
   name: "EmpleadosServiciosOficios",
+  data() {
+    return {
+      oficios: [],
+      empleados: [],
+      oficio: "",
+    };
+  },
+  mounted() {
+    service.getOficios().then((result) => {
+      this.oficios = result;
+    });
+  },
+  methods: {
+    mostrarEmpleados(oficio) {
+      service.getEmpleadosbyOficio(oficio).then((result) => {
+        this.empleados = result;
+      });
+    },
+  },
 };
 </script>
--- src/services/ServiceOficios.js
+++ src/services/ServiceOficios.js
@@ -2,5 +2,27 @@ import Global from './../components/Global';
 import axios from 'axios';
 
 export default class ServiceOficios {
+    getOficios() {
+        return new Promise(function (resolve) {
+            var request = '/api/Empleados/oficios';
+            var url = Global.urlempleados + request;
+            var oficio = [];
+            axios.get(url).then(res => {
+                oficio = res.data;
+                resolve(oficio);
+            });
+        })
+    }
 
+    getEmpleadosbyOficio(oficio) {
+        return new Promise(function (resolve) {
+            var request = '/api/Empleados/EmpleadosOficio/' + oficio;
+            var url = Global.urlempleados + request;
+            var empleados = [];
+            axios.get(url).then(res => {
+                empleados = res.data;
+                resolve(empleados);
+            });
+        })
+    }
 }
